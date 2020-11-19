e5f18bb3bf23f1ed734ca934d3e1b7d5e3a50a62
UCA <UCA@UCA-NAR-WKS-006.UCA.corp>
2020-11-19 23:19:49 +0600

    finished the body page and questions

--- GOT_Quiz/app/src/main/java/com/example/got_quiz/QuizBody.java
+++ GOT_Quiz/app/src/main/java/com/example/got_quiz/QuizBody.java
@@ -14,16 +14,60 @@ public class QuizBody extends AppCompatActivity {
 
 
     String[] options;
+
+    public int state=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_body);
-
+        statusCheck(state);
         RadioGroup radioGroup = findViewById(R.id.Answers);
-        TextView questionTv= findViewById(R.id.QuestionBox);
         Button submitbtn = findViewById(R.id.Submitbtn);
-        options=question1();
-        boolean status =true;
+
+
+
+        submitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int ansindex= radioGroup.getCheckedRadioButtonId();
+                RadioButton radioButton = findViewById(ansindex);
+                String answer= (String) radioButton.getText();
+                if (answer.equals(options[5])){
+                    Toast.makeText(QuizBody.this, "Correct Answer", Toast.LENGTH_SHORT).show();
+                    state+=1;
+                    statusCheck(state);
+                    radioGroup.clearCheck();
+                }
+                else{
+                    Toast.makeText(QuizBody.this, "Wronge Answer", Toast.LENGTH_SHORT).show();
+                    statusCheck(state);
+                }
+            }
+        });
+
+    }
+    public void statusCheck(int state){
+        switch (state){
+            case 0:
+                options=question1();
+                break;
+            case 1:
+                options=question2();
+                break;
+            case 2:
+                options=question3();
+                break;
+            case 3:
+                options=question4();
+                break;
+            case 4:
+                options=question5();
+                break;
+            case 5:
+                options=question6();
+                break;
+        }
+        TextView questionTv= findViewById(R.id.QuestionBox);
         questionTv.setText(options[0]);
         RadioButton answer1 = findViewById(R.id.Answer1);
         answer1.setText(options[1]);
@@ -34,23 +78,7 @@ public class QuizBody extends AppCompatActivity {
         RadioButton answer4 = findViewById(R.id.Answer4);
         answer4.setText(options[4]);
 
-        submitbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int ansindex= radioGroup.getCheckedRadioButtonId();
-                RadioButton radioButton = findViewById(ansindex);
-                String answer= (String) radioButton.getText();
-                if (answer.equals(options[5])){
-                    Toast.makeText(this,"Correct Answer",Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
-
-
-
-    }
-
+    };
 
     private String[] question1(){
         String[] q1={"Besides dragonglass, what is the only other substance capable of defeating White Walkers","Weirwood","Whildfire","Valyrian Steel","Snowballs","Valyrian Steel"};
@@ -63,5 +91,20 @@ public class QuizBody extends AppCompatActivity {
     private String[] question3(){
         return new String[]{"How did Daenerys Targaryen eventually hatch her dragon eggs?","In a lightning storm","In a funeral pyre","In a fireplace","In a frozen cave","In a funeral pyre"};
     }
+    private String[] question4(){
+        return new String[]{"Arya's punishment for stealing from the Many-Face God is: ","Death","Memory Loss","Blindness","Uncontrollable laughter","Blindness" };
+    }
+    private String[] question5(){
+        return new String[]{"What was the name of Ned Stark's greatsword?","Ice","Oathkeeper","Widow's Wail","Northguard","Ice" };
+    }
+    private String[] question6(){
+        return new String[]{"Which Stark family direwolf was killed in retaliation for an attack on Prince Joffrey?","Ghost","Lady","Nymeria","Summer","Lady" };
+    }
+    private String[] question(){
+        return new String[]{"","","","","","" };
+    }
+
+
+
 
 }
